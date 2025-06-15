ac51ba286504051199c140e7a3dfef317b20cb84
omkar dhage <dhageomkar4535@gmail.com>
2025-06-15 18:41:58 +0530

    added method hiding

---
+++ OOP in C sharp/MethodHiding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_in_C_sharp
+{
+
+    class Parent
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Parent Display");
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Parent show");
+        }
+    }
+
+    class child : Parent
+    {
+        //public new void Display()  // For hiding
+        public override void Display()  // for Overriding 
+        {
+            Console.WriteLine("Child Display");
+        }
+        public void show()   // use new keyword to avoid this warning 
+            // using new key we explicitly say that we are entended to do hiding 
+        {
+            Console.WriteLine("Child show");
+        }
+    }
+    internal class MethodHiding
+    {
+        public static void PerformMethodHiding()
+        {
+            Parent p = new child();
+            p.show();   
+            p.Display();    
+        }
+    }
+}
