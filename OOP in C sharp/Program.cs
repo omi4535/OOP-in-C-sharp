0751b721193f4812fcd852df51c715280cfe2e97
omkar dhage <dhageomkar4535@gmail.com>
2025-06-22 00:17:28 +0530

    added delegates with real time example implementation

--- OOP in C sharp/Program.cs
+++ OOP in C sharp/Program.cs
@@ -10,6 +10,11 @@ namespace propgram
             MultipleInheritance.PerformInheritance();
             Overloading.PerformOpOverloading();
             Overloading.performMethodOverLoading();
+
+            WorkIsGoingOn d1 = new WorkIsGoingOn(new Delagetes().WorkIsGoingOn);
+            WorkIsGoingCompleted d2 = new WorkIsGoingCompleted(new Delagetes().WorkIsGoingCompleted);
+            Delagetes.StartWork(5, "study", d1, d2);
+
         }
     }
 
