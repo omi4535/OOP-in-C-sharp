b6dfff0e65a86666dfcd452b313ef4bbca9f220c
omkar dhage <dhageomkar4535@gmail.com>
2025-06-30 00:09:46 +0530

    added multi threading using lock key word

--- OOP in C sharp/Program.cs
+++ OOP in C sharp/Program.cs
@@ -7,15 +7,38 @@ namespace propgram
     {
         public static void Main(string[] args)
         {
-            MultipleInheritance.PerformInheritance();
-            Overloading.PerformOpOverloading();
-            Overloading.performMethodOverLoading();
-            WorkIsGoingOn d1 = new WorkIsGoingOn(new Delagetes().WorkIsGoingOn);
-            WorkIsGoingCompleted d2 = new WorkIsGoingCompleted(new Delagetes().WorkIsGoingCompleted);
-            Delagetes.StartWork(5, "study", d1, d2);
-            MethodHiding.PerformMethodHiding();
-            Generic_Delegates obj = new Generic_Delegates();
-            obj.PerformGenericDelegates();
+            //MultipleInheritance.PerformInheritance();
+
+            //Overloading.PerformOpOverloading();
+
+            //Overloading.performMethodOverLoading();
+
+            //WorkIsGoingOn d1 = new WorkIsGoingOn(new Delagetes().WorkIsGoingOn);
+
+            //WorkIsGoingCompleted d2 = new WorkIsGoingCompleted(new Delagetes().WorkIsGoingCompleted);
+
+            //Delagetes.StartWork(5, "study", d1, d2);
+
+            //MethodHiding.PerformMethodHiding();
+
+            //Generic_Delegates obj = new Generic_Delegates();
+
+            //obj.PerformGenericDelegates();
+
+            Console.WriteLine("Start of main thread");
+            MultiThreading mt = new MultiThreading();
+            mt.PlayWithThread();
+
+            Thread ct = Thread.CurrentThread;
+            if(ct.IsAlive)
+            {
+                Console.WriteLine(ct.Name + "Is alive");
+            }
+
+            MultiThreadingWithCallbacks mtc = new MultiThreadingWithCallbacks();
+            mtc.MultiThreadingCallback(15);
+
+            Console.WriteLine("end of main thread");
         }
     }
 
